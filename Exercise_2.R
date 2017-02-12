18027406c4ff9c83be3fe4277bd3363e3c04c633
Matteo Fumagalli <mfumagalli@users.noreply.github.com>
2017-02-12 15:16:00 +0000

    Create Exercise_2.R

---
+++ Exercise_2.R
@@ -0,0 +1,21 @@
+# Exercise 2 
+
+# TASK A)
+# in a beta-binomial model of allele frequencies, we observe 40 derived alleles (k=40) out of 200 chromosomes (n=200),
+# plot the posterior distribution assuming a uniform (beta) prior.
+# calculate some notable quantiles
+
+# TASK B)
+# as in Task A, but assuming we have only 20 chromosomes (10 individuals), so k=4 and n=20
+
+# TASK C)
+# as in Task A, but using an informative prior. Use a suitable form for the beta distribution that may fit the expected distribution of allele frequencies.
+
+# TASK D)
+# as in Task C, with informative prior but with only 20 chromosomes (10 individuals), so k=4 and n=20
+
+# TASK E)
+# as in Task C, perform a model choice between the cases of the frequency of T<0.1 and the frequency of T>=0.1
+# use a uniform prior, an informative prior as in Tasks C-D, and a skeptical prior
+# calculate Bayes factors
+
